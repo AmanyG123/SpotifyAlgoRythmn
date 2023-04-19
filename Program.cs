b99e56cc0bc9857eb78b2e7671a28e10bf87fda3
AmanyG123 <59115715+AmanyG123@users.noreply.github.com>
2023-04-19 19:30:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+
+namespace SpotifyAPIExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var clientId = "2989616a929a4e0795fa42497949870c";
+            var clientSecret = "80a70ea4bb4f4ff0947b1acf738b6af3";
+
+            var config = SpotifyClientConfig.CreateDefault();
+            var request = new ClientCredentialsRequest(clientId, clientSecret);
+            var response = await new OAuthClient(config).RequestToken(request);
+
+            var spotify = new SpotifyClient(config.WithToken(response.AccessToken));
+
+            // Search for a track
+            var searchRequest = new SearchRequest(SearchRequest.Types.Track, "Bohemian Rhapsody");
+            var searchResult = await spotify.Search.Item(searchRequest);
+
+            if (searchResult.Tracks.Items.Count > 0)
+            {
+                var track = searchResult.Tracks.Items[0];
+                Console.WriteLine($"Found track: {track.Name} by {track.Artists[0].Name}");
+            }
+            else
+            {
+                Console.WriteLine("Track not found.");
+            }
+        }
+    }
+}
